f98e562e0e40dae18172622d197ccd8ecafb2675
Yathamvamsi <vamsiyatham1@gmail.com>
2026-01-28 12:03:49 +0530

    Add files via upload

---
+++ biggest.php
@@ -0,0 +1,14 @@
+<?php
+$a=10;
+$b=60;
+$c=50;
+if(($a>$b) && ($a>$c)){
+echo "a is greater number";
+}
+elseif(($b>$a) && ($b>$c)){
+echo "b is greater number";
+}
+else{
+echo"c is greater number";
+}
+?>
