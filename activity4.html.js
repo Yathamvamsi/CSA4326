0990b83d3b4432ad05279aa8589ae496893ac760
Yathamvamsi <vamsiyatham1@gmail.com>
2026-01-28 12:08:58 +0530

    Add files via upload

---
+++ activity4.html.js
@@ -0,0 +1,12 @@
+<doctype html>
+<html>
+<body>
+<h1 id="display"></h1>
+<script>
+for(let i=1;i<=100;i++){
+document.getElementById("display")=+I;
+document.write("</br>");
+}
+</script>
+</body>
+</html>
